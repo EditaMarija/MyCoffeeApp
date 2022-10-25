02443613925255cf3a5833993738aa6e25cf24b5
Edita Marija <52567746+EditaMarija@users.noreply.github.com>
2022-10-25 12:56:05 +0200

    mvvm helpers

--- MyCoffeeApp/MyCoffeeApp/ViewModels/CoffeeEquipmentViewModel.cs
+++ MyCoffeeApp/MyCoffeeApp/ViewModels/CoffeeEquipmentViewModel.cs
@@ -1,38 +1,74 @@
-﻿using System;
+﻿using MvvmHelpers;
+using MvvmHelpers.Commands;
+using MyCoffeeApp.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
-using Xamarin.Forms;
+
 
 namespace MyCoffeeApp.ViewModels
 {
-    public class CoffeeEquipmentViewModel : BindableObject
+    public class CoffeeEquipmentViewModel : ViewModelBase
     {
+        public ObservableRangeCollection<Coffee> Coffee { get; set; }
+        public ObservableRangeCollection<Grouping<string, Coffee>> CoffeeGroups { get; }
+
+        public AsyncCommand RefreshCommand { get; }
+
         public CoffeeEquipmentViewModel()
         {
-            IncreaseCount = new Command(OnIncrease);
+            Title = "Coffee Equipment";
+            Coffee = new ObservableRangeCollection<Coffee>();
+            CoffeeGroups = new ObservableRangeCollection<Grouping<string, Coffee>>();
+
+            var image = "coffeebag.png";
+            Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Sip of Sunshine", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Potent Potable", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue Bottle", Name = "Kenya Kiambu Handege", Image = image });
+
+            CoffeeGroups.Add(new Grouping<string, Coffee>("Blue Bottle", Coffee.Where(c => c.Roaster == "Blue Bottle")));
+            CoffeeGroups.Add(new Grouping<string, Coffee>("Yes Plz", Coffee.Where(c => c.Roaster == "Yes Plz")));
+
         }
-        public ICommand IncreaseCount { get; }
 
-        int count = 0;
-        string countDisplay = "Click me!";
-
-        public string CountDisplay
+        async Task Refresh()
         {
-            get => countDisplay;
-            set
-            {
-                if (value == countDisplay)
-                    return;
-                countDisplay = value;
-                OnPropertyChanged();
-            }
+            IsBusy = true;
+
+            await Task.Delay(2000);
+
+            IsBusy = false;
         }
 
-        void OnIncrease()
-        {
-            count++;
-            CountDisplay = $"You clicked {count} time(s)!";
-        }
+        //public CoffeeEquipmentViewModel()
+        //{
+        //    IncreaseCount = new Command(OnIncrease);
+        //}
+        //public ICommand IncreaseCount { get; }
+
+        //int count = 0;
+        //string countDisplay = "Click me!";
+
+        //public string CountDisplay
+        //{
+        //    get => countDisplay;
+        //    set => SetProperty(ref countDisplay, value); 
+        //set
+        //{
+        //    if (value == countDisplay)
+        //        return;
+        //    countDisplay = value;
+        //    OnPropertyChanged();
+        //}
+        // }
+
+        //void OnIncrease()
+        //{
+        //    count++;
+        //    CountDisplay = $"You clicked {count} time(s)!";
+        //}
     }
 }
